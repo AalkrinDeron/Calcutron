ac0246b7d486a4498f3694cebf2289665b777a59
Karim Tomaev <AalkrinDeron@gmail.com>
2019-06-14 15:41:27 +0300

    check this

---
+++ MainForm.Designer.cs
@@ -0,0 +1,309 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Karim
+ * Date: 24.09.2017
+ * Time: 16:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Calcutron
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.FontDialog fontDialog1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.ComboBox comboBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.fontDialog1 = new System.Windows.Forms.FontDialog();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(246, 208);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(81, 25);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Выполнить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(521, 208);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(81, 25);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Выполнить";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(6, 28);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(85, 17);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Исходная СС";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(6, 60);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(97, 20);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Необходимая СС";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(6, 91);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(109, 24);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Введите число";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox4);
+			this.groupBox1.Location = new System.Drawing.Point(34, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(293, 59);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Ответ :";
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.SystemColors.InactiveBorder;
+			this.textBox4.Location = new System.Drawing.Point(6, 27);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(281, 20);
+			this.textBox4.TabIndex = 0;
+			this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.textBox3);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Location = new System.Drawing.Point(34, 77);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(293, 125);
+			this.groupBox2.TabIndex = 9;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Параметры :";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(141, 91);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(133, 20);
+			this.textBox3.TabIndex = 10;
+			
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(141, 60);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(133, 20);
+			this.textBox2.TabIndex = 9;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(141, 28);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(133, 20);
+			this.textBox1.TabIndex = 8;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(18, 57);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(73, 20);
+			this.textBox5.TabIndex = 11;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(150, 57);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(73, 20);
+			this.textBox6.TabIndex = 12;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.comboBox1);
+			this.groupBox3.Controls.Add(this.label6);
+			this.groupBox3.Controls.Add(this.label5);
+			this.groupBox3.Controls.Add(this.label1);
+			this.groupBox3.Controls.Add(this.textBox8);
+			this.groupBox3.Controls.Add(this.textBox5);
+			this.groupBox3.Controls.Add(this.textBox6);
+			this.groupBox3.Location = new System.Drawing.Point(333, 77);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(265, 125);
+			this.groupBox3.TabIndex = 13;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Параметры :";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(97, 57);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(47, 21);
+			this.comboBox1.TabIndex = 17;
+			this.comboBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ComboBox1KeyUp);
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(150, 37);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(73, 17);
+			this.label6.TabIndex = 16;
+			this.label6.Text = "Y";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(18, 37);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(73, 17);
+			this.label5.TabIndex = 15;
+			this.label5.Text = "X";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(74, 99);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(109, 23);
+			this.label1.TabIndex = 14;
+			this.label1.Text = "Cистема Cчисления";
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(189, 96);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(70, 20);
+			this.textBox8.TabIndex = 13;
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.textBox7);
+			this.groupBox4.Location = new System.Drawing.Point(333, 12);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(265, 59);
+			this.groupBox4.TabIndex = 14;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Ответ:";
+			// 
+			// textBox7
+			// 
+			this.textBox7.BackColor = System.Drawing.SystemColors.InactiveBorder;
+			this.textBox7.Location = new System.Drawing.Point(6, 27);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.ReadOnly = true;
+			this.textBox7.Size = new System.Drawing.Size(253, 20);
+			this.textBox7.TabIndex = 0;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+			this.ClientSize = new System.Drawing.Size(638, 241);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Calcutron";
+			this.Shown += new System.EventHandler(this.MainFormShown);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+		}
+		}
---
+++ MainForm.cs
@@ -0,0 +1,377 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Karim
+ * Date: 24.09.2017
+ * Time: 16:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Calcutron
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		 //Перевод строки в числовой список
+		
+        public static List <double> NumTr(string ss,string lfa) //Введенное число\строка->List!!!ALAM
+        {
+            var aal =new List<double>();
+            for (int i=0;i<ss.Length;i++)
+                aal.Add(lfa.IndexOf(ss[i]));                
+            return aal; 
+        }
+        
+        public static string TrNum(List<double>les,string lfa)//List->Num !!!!ALAM превращение результата в вывод
+        {
+            string s1="";
+            int Hella=0;
+            for(int i=0;i<les.Count;i++)
+            {
+                Hella=Convert.ToInt32(les[i]);
+                s1=s1+lfa[Hella];
+            }
+            return s1;   
+        }
+        
+        public static double TT(List<double> num,double System_Pas)//To Ten
+        {       
+            double res = 0 ;
+            int gon = 0;
+            bool minus=false;
+            if(num[0]==37)
+            {
+                num.Remove(37);
+                minus=true;
+            }
+            int j = num.Count;
+            for (int i=0;i<num.Count;i++)
+                if (num[i]==36)
+                j=i;
+            while(gon!=num.Count)
+            {
+                if (num[gon]!=36)//if num[0]=37-bool=true=>0-res
+                {   
+                    res=res+num[gon]*Math.Pow(System_Pas,j-1);      
+                    j--;
+                }
+                gon ++;
+            }
+            if(minus==true)
+            	res=0-res;            
+            return res;
+        }
+        
+        public static string reload(double num,int sys,string result,string Alfa)//Tail Trans !!!ALAM result идет снаружи
+		{
+			double act;
+			int colt;	
+			result =".";
+			for(int i=0;i<10;i++)
+			{
+			    act=num*sys;			    
+			    colt=Convert.ToInt32(act);
+			    
+			    if(colt>act)
+			    {
+			    	num=act-(colt-1);
+			        result=result+Alfa[colt-1];
+			    }
+			    if(colt<act)
+			    {
+			     	num=act-colt;
+			     	result=result+Alfa[colt];
+			    }
+			    if(colt==act) //если остаток больше числа
+			    {
+			    	result=result+Alfa[colt];			    
+			    	break;
+			    }
+			    
+			}
+			return result;
+			
+		}
+        
+		public static string FT(double num,int System_Fut,string Lfa)//From Ten no tail
+        {
+            var res =new List<double>();
+            int helpr;
+            string res1="";
+			string res2="";
+			double num1=0;
+			int colts=Math.Abs(Convert.ToInt32(num));	
+			double nummy=0;
+			if (num<0)
+			{
+				nummy= num;
+				num=num*(-1);
+			}
+			if(num>0)
+				if(colts>num)
+            		colts=colts-1;
+			if(num<0)
+				if(colts<num)
+					colts=colts+1;
+			num1=num-colts;
+			num=num-num1;
+			if(num==0)
+				res.Add(0);
+            while(num!=0)
+            {
+                res.Insert(0,(num % System_Fut));
+                helpr=Convert.ToInt32(num);
+                num=helpr/System_Fut;                
+            }
+            if (nummy!=0)
+            	res.Insert(0,37);
+            for(int i =0;i<res.Count;i++)
+            	res1=TrNum(res,Lfa);            
+            if(num1!=0)
+            {
+            	res2=reload(num1,System_Fut,res2,Lfa);
+            	res1=res1+res2;
+            }
+            return res1;
+        }
+					
+		public static string Act(string x, string y,string syst,string sign,string Lfa)
+		{
+			var a =new List<double>();
+			var b =new List<double>();
+			double c;
+			double d;
+			int System_Pas=Convert.ToInt32(syst);
+			a=NumTr(x,Lfa);
+			b=NumTr(y,Lfa);
+			c=TT(a,System_Pas);
+			d=TT(b,System_Pas);
+			switch (sign)
+            {
+				case "+":
+					c=c+d;
+					break;
+				case "-":
+					c=c-d;
+					break;
+				case "/":
+					c=c/d;
+					break;
+				case "*":
+					c=c*d;
+					break;
+            }
+			
+			string result=FT(c,System_Pas,Lfa);
+			return result;
+		}
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//string Alfa="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.-";
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			
+		}
+		
+		bool Check_Syst(string text,string Lfa,bool Er,string num)
+		{ 
+			int b = 0;
+			bool n1=false;
+			for(int i=0;i<text.Length;i++)
+			
+				if (num.IndexOf(text[i])==-1)
+			    {   
+				    MessageBox.Show("Имеются недопустимые символы");
+					Er=true;
+					n1=true;
+					return Er;
+			}
+			
+			
+			for(int i=0;i<text.Length;i++)
+			{
+				if (Lfa.IndexOf(text[i])==-1 || Lfa.IndexOf(text[i])>9 )
+				{
+					MessageBox.Show("Имеются недопустимые символы");
+					Er=true;
+					return Er;
+				}
+			}
+			for(int i=0;i<text.Length;i++)
+			{
+				if (n1==false)
+				{
+				b=Convert.ToInt32(text);
+				if (b>36 || b<2 )
+				{
+					MessageBox.Show("Система счисления выходит за пределы интервалов [2;36]");
+					Er=true;
+					return Er;
+				}
+				}
+			}
+			
+           
+           
+			
+		  return Er;
+		}
+		
+		bool Check_Count(string text,string Lfa,bool Er,string Sys,string num,int z)
+		{ 
+			bool n1=false;
+			int f;
+		
+			for(int i=0;i<text.Length;i++)
+			{
+				
+				if (Lfa.IndexOf(text[i])==-1 )
+				{
+					MessageBox.Show("Имеются недопустимые символы");
+					Er=true;
+					return Er;
+					
+				}
+				if (Lfa.IndexOf(text[0])==36 || Lfa.IndexOf(text[text.Length-1])==36 || text.IndexOf("-") > 0 )
+				{
+					MessageBox.Show("Число записано неверно");
+					Er=true;
+					return Er;
+
+				}
+				if (z==1)
+				{
+					f=Convert.ToInt32(Sys);
+					if (Lfa.IndexOf(text[i])>=f && Lfa.IndexOf(text[i])!=36 && Lfa.IndexOf(text[i])!=37)
+				    {
+					MessageBox.Show("Неверно указана система счисления");
+					Er=true;
+					return Er;
+				    }
+				}
+					
+			}
+		  return Er;
+		}
+		
+		void Button1Click(object sender, EventArgs e)//check
+		{
+			string Numeric="0123456789";
+			string Alfa="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.-";
+			bool Err = false;
+			Err=Check_Syst(textBox1.Text,Alfa,Err,Numeric);
+			if (Err==true)
+			{   
+				textBox1.Clear();
+				textBox2.Clear();
+				textBox3.Clear();
+				textBox4.Text="Error";
+				return;
+			}
+			Err=Check_Syst(textBox2.Text,Alfa,Err,Numeric);
+			if (Err==true)
+			{   
+				textBox1.Clear();
+				textBox2.Clear();
+				textBox3.Clear();
+				textBox4.Text="Error";
+				return;
+			}
+			Err=Check_Count(textBox3.Text,Alfa,Err,textBox1.Text,Numeric,1);
+			if (Err==true)
+			{   
+				textBox1.Clear();
+				textBox2.Clear();
+				textBox3.Clear();
+				textBox4.Text="Error";
+				return;
+			}
+			Err=Check_Count(textBox3.Text,Alfa,Err,textBox2.Text,Numeric,2);
+				
+			if (Err==true)
+			{   
+				textBox1.Clear();
+				textBox2.Clear();
+				textBox3.Clear();
+				textBox4.Text="Error";
+				return;
+			}
+			var Num =new List<double>();
+			double D_Num;	
+			int Sys_Pas=Convert.ToInt32(textBox1.Text);
+			Num=NumTr(textBox3.Text,Alfa);
+			D_Num=TT(Num,Sys_Pas);
+			Sys_Pas=Convert.ToInt32(textBox2.Text);
+			textBox4.Text=FT(D_Num,Sys_Pas,Alfa);
+			
+			
+	    }
+		void Button2Click(object sender, EventArgs e)
+		{
+			bool Err = false;
+			string Numeric="0123456789";
+			string Alfa="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.-";
+			Err=Check_Syst(textBox8.Text,Alfa,Err,Numeric);
+			if (Err==true)
+			{   
+				textBox5.Clear();
+				textBox6.Clear();
+				textBox8.Clear();
+				textBox7.Text="Error";
+				return;
+			}
+			Err=Check_Count(textBox5.Text,Alfa,Err,textBox8.Text,Numeric,1);
+			if (Err==true)
+			{   
+				textBox5.Clear();
+				textBox6.Clear();
+				textBox8.Clear();
+				textBox7.Text="Error";
+				return;
+			}
+			Err=Check_Count(textBox6.Text,Alfa,Err,textBox8.Text,Numeric,1);
+				
+			if (Err==true)
+			{   
+				textBox5.Clear();
+				textBox6.Clear();
+				textBox8.Clear();
+				textBox7.Text="Error";
+				return;
+			}
+			
+			textBox7.Text=Act(textBox5.Text,textBox6.Text,textBox8.Text,comboBox1.Text,Alfa);
+		}
+		void MainFormShown(object sender, EventArgs e)
+		{
+			
+			comboBox1.Items.Add("+");
+			comboBox1.Items.Add("-");
+			comboBox1.Items.Add("/");
+			comboBox1.Items.Add("*");
+		}
+		void ComboBox1KeyUp(object sender, KeyEventArgs e)
+		{
+		    MessageBox.Show("Имеются недопустимые символы");
+			comboBox1.Text="";
+	
+		}
+	
+		
+	
+		
+		
+	}
+}
